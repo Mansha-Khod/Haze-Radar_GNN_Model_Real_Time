d5e35451a515956cf494d39892dc98b88a8d12de
Mansha-Khod <manshakhod@gmail.com>
2026-01-17 19:15:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -686,5 +686,5 @@ async def get_cities():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 8000))  
     uvicorn.run(app, host="0.0.0.0", port=port)
